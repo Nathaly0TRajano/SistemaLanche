0f0de5965575f334e5603d4f531ed62e9ecaf36a
Nathaly0TRajano <nathydelima00@gmail.com>
2025-04-01 15:11:09 -0300

    Acessos do funcionario

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Livewire\Cliente\Create;
 use App\Livewire\Funcionario\Create as FuncionarioCreate;
+use App\Livewire\Funcionario\Dashboard;
 use App\Livewire\Produto\Create as ProdutoCreate;
 use Illuminate\Support\Facades\Route;
 
 Route::get('cadastro/cliente', Create::class);
 
-Route::get('funcionario/dashboard/cadastro/produto', ProdutoCreate::class);
+Route::get('funcionario/dashboard/cadastro/produto', ProdutoCreate::class)->middleware(['auth', 'role:funcionario']);
+
+Route::get('/funcionario/dashboard', Dashboard::class)->middleware(['auth', 'role:funcionario']);
 
 Route::get('cadastro/funcionario', FuncionarioCreate::class);
